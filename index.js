816442a56df538e269a2ec083fc9fe5dfd16b85e
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-08-28 22:58:07 +0600

    css setup

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function loadTodos() {
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  return todos;
+}
+
+function addTodoToLocalStorage(todoText) {
+  const todos = loadTodos();
+  todos.push(todoText);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function appendTodoInHtmal() {
+  const todoList = document.getElementById("todoList");
+  const todo = document.createElement("li");
+  todoList.appendChild(todo);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -10,9 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   submitButton.addEventListener("click", (event) => {
     const todoText = todoInput.value;
-
-    if (todoText === " ") {
+    if (todoText == "") {
       alert("please somthings for the todo");
+    } else {
+      addTodoToLocalStorage(todoText);
     }
   });
 
